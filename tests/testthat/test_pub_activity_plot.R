b4b0b126e985f4290b7549b9aed1e4835b64467e
Marina Leer <123594503+marina-leer@users.noreply.github.com>
2024-10-05 20:23:43 +0200

    Update test_pub_activity_plot.R

--- tests/testthat/test_pub_activity_plot.R
+++ tests/testthat/test_pub_activity_plot.R
@@ -1,4 +1,4 @@
-test_that("Test function ran without error", {
+test_that("Test - function ran without error", {
 
     gene_list_mouse  = c("Mbl1")
     keyword_list = c("stem cell")
@@ -13,3 +13,17 @@ test_that("Test function ran without error", {
 
 })
 
+
+test_that("Test - function ran when keyword set empty", {
+
+  gene_list_mouse  = "Mbl1"
+  keyword_list = ""
+
+  result2 <- pub_activity_plot(gene_list_mouse,
+                               keyword_list,
+                               species = "mouse",
+                               output_dir=NULL,
+                               show_progressbar=FALSE)
+
+  expect_identical(result2, 1)
+})
