71444a4c1d06c3f55c57018d429275b9228d9a3c
Georgeloy <georgiossoultoukis675@hotmail.com>
2024-10-06 19:39:08 +0200

    Create folder for example run
    Validation of biological context computation using the Gene Ontology (GO) system

---
+++ paper/example_test_validation_of_biological_context/KS_DSBP.R
@@ -0,0 +1,16 @@
+
+# library(biomaRt)
+# library(rentrez)
+# library(png)
+# install.packages("GeSciLiVis")
+library("GeSciLiVis")
+
+gene_list_mouse <- c("Dse",
+                     "Chst14",
+                     "Chst12"
+)
+
+keyword_list1 <- c("Keratan sulfate")
+
+pub_activity_plot(g_list = gene_list_mouse, k_list = keyword_list1, species = "mouse",output_dir = "DSbiosynth_KWDS")
+
